794fd36158d625e5c8c1f40194809801f289992e
MrNivorous <karn.sood@gmail.com>
2013-03-20 22:08:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnergyChomper::Application.config.secret_token = '48183d71f66c86fc32ea5d4878cc34b4bd00ffe3e40c96e267a2c297c48f481ddb3e8b79a2f8cb777b65699c1da0a11688b7a8756c2463196128966b15b9c2f1'
